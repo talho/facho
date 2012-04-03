a330c6e09662740f320c4297c66a02623777b9d8
cdubose <cdubose@marlin.(none)>
2012-04-03 09:47:33 -0500

    fixing deprecations for upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :facho_users, :as=> 'facho/users', :controller=> 'facho/users', :only=> [:new, :create]
+Openphin::Application.routes.draw do
+  namespace :facho do
+    resources :users, :only=> [:new, :create]
+  end
 end
