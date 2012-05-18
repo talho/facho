f122305cd03bb8e1dfb07285232c3a91a74f145a
cdubose <cdubose@marlin.(none)>
2012-05-18 10:04:19 -0500

    clearing things up for facho specifications

--- db/fixtures/roles.rb
+++ db/fixtures/roles.rb
@@ -3,7 +3,5 @@ r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_applicat
 p "#{r.name} - #{r.application}"
 r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_application('Admin', true, false, 'facho')
 p "#{r.name} - #{r.application}"
-r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_application('Staff', true, true, 'facho')
-p "#{r.name} - #{r.application}"
-r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_application('Director', true, true, 'facho')
+r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_application('Health Oficer', true, true, 'facho')
 p "#{r.name} - #{r.application}"
