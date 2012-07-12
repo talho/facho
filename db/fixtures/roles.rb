69ddbbe7b7d116317aa602f2a7de5c04afda36a8
cdubose <cdubose@marlin.(none)>
2012-07-12 16:22:16 -0500

    Prepping facho for the app manager

--- db/fixtures/roles.rb
+++ db/fixtures/roles.rb
@@ -1,10 +1,10 @@
 app = App.find_by_name('facho')
 p 'Writing Roles'
-r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_app_id('Dashboard', false, false, app.id) do |role|
+r = Role.find_or_create_by_name_and_user_role_and_app_id('Dashboard', false, app.id) do |role|
   role.public = true
 end
 p "#{r.name} - #{r.application}"
-r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_app_id('Admin', true, false, app.id)
+r = Role.find_or_create_by_name_and_user_role_and_app_id('Admin', false, app.id)
 p "#{r.name} - #{r.application}"
-r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_app_id('Health Oficer', true, true, app.id)
+r = Role.find_or_create_by_name_and_user_role_and_app_id('Health Oficer', true, app.id)
 p "#{r.name} - #{r.application}"
