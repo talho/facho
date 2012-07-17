b36a73a7be6c84f8e714387f12a62df249d6d2e7
cdubose <cdubose@marlin.(none)>
2012-07-17 09:10:35 -0500

    Making facho a railtie instead of letting it float

--- lib/facho.rb
+++ lib/facho.rb
@@ -1,7 +1,8 @@
-
 # Tell the main app that this extension exists
 $extensions = [] unless defined?($extensions)
 $extensions << :facho
 
 module Facho
 end
+
+require 'facho/railtie'
---
+++ lib/facho/railtie.rb
@@ -0,0 +1,5 @@
+class Facho::Railtie < Rails::Railtie
+  rake_tasks do
+    Dir[File.join(File.dirname(__FILE__),'..','tasks/*.rake')].each { |f| load f }
+  end
+end
