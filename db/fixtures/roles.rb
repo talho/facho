c9863878ad36adb61805cfc3e68fc05f1ec9d5cd
cdubose <cdubose@marlin.(none)>
2012-10-03 15:51:10 -0500

    spelling error

--- db/fixtures/roles.rb
+++ db/fixtures/roles.rb
@@ -6,5 +6,5 @@ end
 p "#{r.name} - #{r.application}"
 r = Role.find_or_create_by_name_and_user_role_and_app_id('Admin', false, app.id)
 p "#{r.name} - #{r.application}"
-r = Role.find_or_create_by_name_and_user_role_and_app_id('Health Oficer', true, app.id)
+r = Role.find_or_create_by_name_and_user_role_and_app_id('Health Officer', true, app.id)
 p "#{r.name} - #{r.application}"
